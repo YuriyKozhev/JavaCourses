86b12e76acc6d02d807865f210ccfda9248c23e9
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-11 12:54:38 +0300

    add arrays

--- src/com/yuriy/Main.java
+++ src/com/yuriy/Main.java
@@ -1,18 +1,19 @@
 package com.yuriy;
 
 import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Date;
 //import org.apache.commons.lang.SerializationUtils;
 
 public class Main {
 
     public static void main(String[] args) {
-        Point point1 = new Point(1, 2);
-        Point point2 = new Point();
-        point1.x = 2;
-        String name = point2.toString();
-        String modified = name.replace("]", ")");
-        int cond = modified.indexOf("]");
-        System.out.println("Hello World " + modified + cond);
+        int[][] numbers = new int[5][2];
+        numbers[4][0] = 1;
+        System.out.println(Arrays.deepToString(numbers));
+        int[][] figures = {{ 3, 1, 2 }, { 1, 2}};
+//        Arrays.sort(figures);
+        System.out.println(Arrays.deepToString(figures));
     }
 }
