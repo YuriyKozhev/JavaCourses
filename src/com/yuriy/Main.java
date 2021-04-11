0df088d2f60e21576684111252c538335602c820
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-11 12:07:17 +0300

    primitive types

--- src/com/yuriy/Main.java
+++ src/com/yuriy/Main.java
@@ -3,6 +3,14 @@ package com.yuriy;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int age = 30;
+        age = 25;
+        int viewsCount = 7_231_455;
+        float price = 10.33F;
+        double newPrice = 10.33;
+        double finalPrice = price + newPrice;
+        char letter = 'F';
+        boolean isAlive = false;
+        System.out.println("Hello World " + isAlive);
     }
 }
