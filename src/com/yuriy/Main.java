9892cfdf6ea0f845a3abc1c9f9c6329dbebb04c5
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-11 17:52:45 +0300

    add artifact build

--- src/com/yuriy/Main.java
+++ src/com/yuriy/Main.java
@@ -4,15 +4,8 @@ package com.yuriy;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start");
-        printNumbers(4);
-        System.out.println("Finish");
+        System.out.println("Hello World");
     }
 
-    public static void printNumbers(int limit) {
-        for (int i = 0; i < limit; i += 2) {
-            System.out.println(i);
-        }
-    }
 
 }
