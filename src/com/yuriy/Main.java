7a420e5244360542350b502115dc22af4abe799f
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-11 17:25:07 +0300

    create separate methods for calculating balance, monthly rate and period in month

--- src/com/yuriy/Main.java
+++ src/com/yuriy/Main.java
@@ -7,6 +7,8 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
+    final static byte MONTH_IN_YEAR = 12;
+    final static byte PERCENT = 100;
 
     public static double readNumber(
             Scanner scanner,
@@ -32,36 +34,48 @@ public class Main {
         double annualRate = readNumber(scanner, "Annual Rate: ", 1, 30);
         int period = (int) readNumber(scanner, "Period: ", 1, 30);
 
-        double mortgage = calculateMortgage(principal, annualRate, period);
-
         System.out.println("Mortgage");
         System.out.println("________");
+        double mortgage = calculateMortgage(principal, annualRate, period);
         System.out.println("Monthly Payments: " + NumberFormat.getCurrencyInstance().format(mortgage));
 
         System.out.println("Payment Schedule");
         System.out.println("________");
-
-        double monthlyRate = annualRate / 100 / 12;
-        int periodMonths = period * 12;
-
+        int periodMonths = calculatePeriodMonths(period);
         for (int i = 1; i <= periodMonths; i++) {
-            double B = principal * (Math.pow(1 + monthlyRate, periodMonths)
-                    - Math.pow(1 + monthlyRate, i)) / (Math.pow(1 + monthlyRate, periodMonths) - 1);
-            System.out.println(NumberFormat.getCurrencyInstance().format(B));
+            double balance = calculateBalance(principal, annualRate, periodMonths, i);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
         }
+
     }
 
-//    public static double calculateMonthlyRate()
+    public static double calculateBalance(
+            double principal,
+            double annualRate,
+            int periodMonths,
+            int numberOfPaymentsMade) {
+        double monthlyRate = calculateMonthlyRate(annualRate);
+
+        return principal
+                * (Math.pow(1 + monthlyRate, periodMonths)
+                    - Math.pow(1 + monthlyRate, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyRate, periodMonths) - 1);
+    }
+
+    public static double calculateMonthlyRate(double annualRate) {
+        return annualRate / PERCENT / MONTH_IN_YEAR;
+    }
+
+    public static int calculatePeriodMonths(int period) {
+        return period * MONTH_IN_YEAR;
+    }
 
     public static double calculateMortgage(
             double principal,
             double annualRate,
             int period) {
-        final byte MONTH_IN_YEAR = 12;
-        final byte PERCENT = 100;
-
-        double monthlyRate = annualRate / PERCENT / MONTH_IN_YEAR;
-        int periodMonths = period * MONTH_IN_YEAR;
+        double monthlyRate = calculateMonthlyRate(annualRate);
+        int periodMonths = calculatePeriodMonths(period);
 
         double mortgage = principal *
                 monthlyRate * Math.pow(1 + monthlyRate, periodMonths) /
