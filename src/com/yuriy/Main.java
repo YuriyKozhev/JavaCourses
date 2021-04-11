1a963f5eb90d1a8b5ffb5daca5ae022cbd7a3cd5
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-11 14:13:46 +0300

    add logical expressions

--- src/com/yuriy/Main.java
+++ src/com/yuriy/Main.java
@@ -9,26 +9,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        final byte MONTH_IN_YEAR = 12;
-        final byte PERCENT = 100;
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        double principal = scanner.nextDouble();
-
-        System.out.print("Annual interest rate: ");
-        double annualRate = scanner.nextDouble();
-
-        System.out.print("Period (Years): ");
-        int period = scanner.nextInt();
-
-        double monthlyRate = annualRate / PERCENT / MONTH_IN_YEAR;
-        double periodMonths = period * MONTH_IN_YEAR;
-
-        double mortgage = principal *
-                monthlyRate * Math.pow(1 + monthlyRate, periodMonths) /
-                (Math.pow(1 + monthlyRate, periodMonths) - 1);
-
-        System.out.println("Mortgage " + NumberFormat.getCurrencyInstance().format(mortgage));
+        int x = 1;
+        int y = 1;
+        boolean[] array = {true, false};
+        System.out.println(x == y || array[4]);
     }
 }
