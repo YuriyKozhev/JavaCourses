dde1c4dfa1451376b76f581257d55cfa16e48d20
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-11 13:12:10 +0300

    add casting

--- src/com/yuriy/Main.java
+++ src/com/yuriy/Main.java
@@ -1,19 +1,12 @@
 package com.yuriy;
 
-import java.awt.*;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Date;
 //import org.apache.commons.lang.SerializationUtils;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[][] numbers = new int[5][2];
-        numbers[4][0] = 1;
-        System.out.println(Arrays.deepToString(numbers));
-        int[][] figures = {{ 3, 1, 2 }, { 1, 2}};
-//        Arrays.sort(figures);
-        System.out.println(Arrays.deepToString(figures));
+        double result = (double) 3 / 2;
+        int intResult = (int) result + Integer.parseInt("2");
+        System.out.println(intResult);
     }
 }
