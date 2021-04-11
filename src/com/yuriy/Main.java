c2efabf4fa74377a352c93f59828b8a54eb83441
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-11 12:12:06 +0300

    reference types

--- src/com/yuriy/Main.java
+++ src/com/yuriy/Main.java
@@ -1,16 +1,15 @@
 package com.yuriy;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
         int age = 30;
-        age = 25;
-        int viewsCount = 7_231_455;
-        float price = 10.33F;
-        double newPrice = 10.33;
-        double finalPrice = price + newPrice;
-        char letter = 'F';
-        boolean isAlive = false;
-        System.out.println("Hello World " + isAlive);
+        for (int fd = 0; fd < age; fd++) {
+
+        }
+        Date now = new Date();
+        System.out.println("Hello World " + now);
     }
 }
