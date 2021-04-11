1c9172c0ea38a495054b35ae4f72ff527881067f
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-11 14:14:12 +0300

    add ternary operator

--- src/com/yuriy/Main.java
+++ src/com/yuriy/Main.java
@@ -9,9 +9,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int x = 1;
-        int y = 1;
-        boolean[] array = {true, false};
-        System.out.println(x == y || array[4]);
+        double income = 120_000;
+        String isHigh = income > 100_000 ? "Yes" : "No";
+        System.out.println(isHigh);
     }
 }
