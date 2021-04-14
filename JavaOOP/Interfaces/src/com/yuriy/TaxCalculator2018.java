7866dc581af7a2704b7202807028c483dd4056af
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-14 17:55:32 +0300

    rename TaxCalculator to TaxCalculator2018 and create interface TaxCalculator

---
+++ JavaOOP/Interfaces/src/com/yuriy/TaxCalculator2018.java
@@ -0,0 +1,13 @@
+package com.yuriy;
+
+public class TaxCalculator2018 {
+    private double taxableIncome;
+
+    public TaxCalculator2018(double taxableIncome) {
+        this.taxableIncome = taxableIncome;
+    }
+
+    public double calculateTax() {
+        return taxableIncome * 0.3;
+    }
+}
