f744c3e3580080bf2748507d3bfd9f57e3b30dde
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-14 14:42:21 +0300

    create field currencyInstance in MortageReport class

--- JavaOOP/2/src/com/yuriy/MortageReport.java
+++ JavaOOP/2/src/com/yuriy/MortageReport.java
@@ -3,18 +3,21 @@ package com.yuriy;
 import java.text.NumberFormat;
 
 public class MortageReport {
+
+    private static final NumberFormat currencyInstance = NumberFormat.getCurrencyInstance();
+
     public static void printMorgage(MortgageCalculator mortgageCalculator) {
         System.out.println("Mortgage");
         System.out.println("________");
         double mortgage = mortgageCalculator.calculateMortgage();
-        System.out.println("Monthly Payments: " + NumberFormat.getCurrencyInstance().format(mortgage));
+        System.out.println("Monthly Payments: " + currencyInstance.format(mortgage));
     }
 
     public static void printPaymentSchedule(MortgageCalculator mortgageCalculator) {
         System.out.println("Payment Schedule");
         System.out.println("________");
         for (double balance : mortgageCalculator.getRemainingBalances()) {
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+            System.out.println(currencyInstance.format(balance));
         }
     }
 }
