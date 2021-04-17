e58f50c51c49f375c83fcdaffbd22f63f23d6345
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-17 12:21:38 +0300

    catch FileNotFoundException exception

--- Exceptions/src/com/yuriy/ExceptionsDemo.java
+++ Exceptions/src/com/yuriy/ExceptionsDemo.java
@@ -1,11 +1,16 @@
 package com.yuriy;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ExceptionsDemo {
     public static void show() {
-        sayHello(null);
+        FileReader reader;
+        try {
+            reader = new FileReader("file.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void sayHello(String name) {
-        System.out.println(name.toUpperCase());
-    }
 }
