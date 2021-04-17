fd312952d8e189a9b026b9bbee1c8819c4a08e73
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-17 13:05:39 +0300

    add throw of IllegalArgumentException

---
+++ Exceptions/src/com/yuriy/Account.java
@@ -0,0 +1,9 @@
+package com.yuriy;
+
+public class Account {
+    public void deposit(float value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
--- Exceptions/src/com/yuriy/ExceptionsDemo.java
+++ Exceptions/src/com/yuriy/ExceptionsDemo.java
@@ -1,24 +1,10 @@
 package com.yuriy;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
-        try (
-                var reader = new FileReader("file.txt");
-                var writer = new FileWriter("other.txt");
-        ){
-            var value = reader.read();
-            new SimpleDateFormat().parse("");
-        } catch (IOException | ParseException e) {
-            System.out.println("Unable to read or parse data");
-        }
-
+        Account account = new Account();
+        account.deposit(-1);
     }
 
 }
