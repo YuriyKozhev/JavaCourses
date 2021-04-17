705f6cf7526fbd03087baf8f1e5b18325a53676c
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-17 12:38:26 +0300

    add catch block for IOException

--- Exceptions/src/com/yuriy/ExceptionsDemo.java
+++ Exceptions/src/com/yuriy/ExceptionsDemo.java
@@ -2,14 +2,18 @@ package com.yuriy;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class ExceptionsDemo {
     public static void show() {
         FileReader reader;
         try {
             reader = new FileReader("file.txt");
+            var value = reader.read();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Unable to read data");
         }
     }
 
