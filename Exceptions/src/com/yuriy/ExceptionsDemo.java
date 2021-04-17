f3803a045c62fc932db1b2f682488657833179a4
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-17 12:51:27 +0300

    add try-with-resources block

--- Exceptions/src/com/yuriy/ExceptionsDemo.java
+++ Exceptions/src/com/yuriy/ExceptionsDemo.java
@@ -2,28 +2,23 @@ package com.yuriy;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
-        FileReader reader = null;
-        try {
-            reader = new FileReader("file.txt");
+        try (
+                var reader = new FileReader("file.txt");
+                var writer = new FileWriter("other.txt");
+        ){
             var value = reader.read();
             new SimpleDateFormat().parse("");
         } catch (IOException | ParseException e) {
             System.out.println("Unable to read or parse data");
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
+
     }
 
 }
