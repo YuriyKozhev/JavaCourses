e7ca318b1c5454f0227379a285dd47baf47972ac
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-17 13:21:56 +0300

    add withdraw method that throws InsufficientFundsException

--- Exceptions/src/com/yuriy/Account.java
+++ Exceptions/src/com/yuriy/Account.java
@@ -3,9 +3,16 @@ package com.yuriy;
 import java.io.IOException;
 
 public class Account {
+    private float deposit = 0;
+
     public void deposit(float value) throws IOException {
         if (value <= 0) {
             throw new IOException();
         }
+    }
+
+    public void withdraw(float value) throws InsufficientFundsException {
+        if (deposit < value)
+            throw new InsufficientFundsException();
     }
 }
