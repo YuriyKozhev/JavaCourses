c6fed518da2fad217105f2d874e0fe76c03d0d69
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-19 00:47:27 +0300

    create GenericList class implementing Iterable interface

--- Collections/src/com/yuriy/Main.java
+++ Collections/src/com/yuriy/Main.java
@@ -3,6 +3,12 @@ package com.yuriy;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        GenericList<String> list = new GenericList<>();
+        list.add("a");
+        list.add("b");
+
+        for (var item : list) {
+            System.out.println(item);
+        }
     }
 }
