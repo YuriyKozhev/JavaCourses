5d44102ec62acf2ac2990e1db94c8d16e3e180d8
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-18 14:42:48 +0300

    add multiple type parameters

--- Generics/src/com/yuriy/Main.java
+++ Generics/src/com/yuriy/Main.java
@@ -3,9 +3,6 @@ package com.yuriy;
 public class Main {
 
     public static void main(String[] args) {
-        User user1 = new User(10);
-        User user2 = new User(20);
-
-        System.out.println(Utils.max(user1, user2));
+        Utils.print("John", 10);
     }
 }
