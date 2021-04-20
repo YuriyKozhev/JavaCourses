ee56369699b29d789f013616d4c95a4445f9fa92
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-20 17:47:17 +0300

    add UnaryOperator usages

--- Lambdas/src/com/yuriy/LambdasDemo.java
+++ Lambdas/src/com/yuriy/LambdasDemo.java
@@ -5,13 +5,8 @@ import java.util.function.*;
 
 public class LambdasDemo {
     public static void show() {
-        BinaryOperator<Integer> add = (a, b) -> a + b;
-        System.out.println(add.apply(1, 2));
-
-        IntBinaryOperator add2 = Integer::sum;
-        System.out.println(add2.applyAsInt(1, 2));
-
-        Function<Integer, Integer> square = a -> a * a;
-        System.out.println(square.apply(add2.applyAsInt(1, 2)));
+        UnaryOperator<Integer> square = n -> n * n;
+        UnaryOperator<Integer> increment = n -> n + 1;
+        System.out.println(increment.andThen(square).apply(2));
     }
 }
