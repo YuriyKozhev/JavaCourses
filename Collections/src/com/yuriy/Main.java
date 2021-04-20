611673b4da9dc0fdecc9a1e7fe22f421db29dfb6
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-20 11:05:01 +0300

    add example of list sorting

--- Collections/src/com/yuriy/Main.java
+++ Collections/src/com/yuriy/Main.java
@@ -1,8 +1,19 @@
 package com.yuriy;
 
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        ListDemo.show();
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("a"));
+        customers.add(new Customer("c"));
+        customers.add(new Customer("b"));
+        Collections.sort(customers);
+        System.out.println(customers);
     }
 }
