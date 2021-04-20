c0f67ea6d8b1c4c2db6cffc5f8b0e2e54d86cd89
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-20 11:19:13 +0300

    add examples of using Queue with implementation of ArrayDeque

--- Collections/src/com/yuriy/Main.java
+++ Collections/src/com/yuriy/Main.java
@@ -7,11 +7,6 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<Customer> customers = new ArrayList<>();
-        customers.add(new Customer("a", "e3"));
-        customers.add(new Customer("c", "e2"));
-        customers.add(new Customer("b", "e1"));
-        Collections.sort(customers, new EmailComparator());
-        System.out.println(customers);
+        QueueDemo.show();
     }
 }
