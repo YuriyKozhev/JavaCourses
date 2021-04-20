07b73f864c7ed732a600dd7ac16a1d0ebb988580
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-20 17:34:44 +0300

    add Predicate example

--- Lambdas/src/com/yuriy/LambdasDemo.java
+++ Lambdas/src/com/yuriy/LambdasDemo.java
@@ -3,20 +3,12 @@ package com.yuriy;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class LambdasDemo {
     public static void show() {
-        // "key:value" -> "key=value" -> "{key=value}"
-        Function<String, String> replaceColon = str -> str.replace(':','=');
-        Function<String, String> addBraces = str -> "{" + str + "}";
-
-        System.out.println(replaceColon
-                .andThen(addBraces)
-                .apply("key:value"));
-
-        System.out.println(addBraces
-                .compose(replaceColon)
-                .apply("key:value"));
+        Predicate<String> isLongerThan5 = str -> str.length() > 5;
+        System.out.println(isLongerThan5.test("you"));
     }
 }
