833b668bff8218a9ae892cfed479cdd195c017f1
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-27 23:59:17 +0300

    add Stream flatMap method example

--- Streams/src/com/yuriy/StreamsDemo.java
+++ Streams/src/com/yuriy/StreamsDemo.java
@@ -8,14 +8,9 @@ import java.util.stream.Stream;
 
 public class StreamsDemo {
     public static void show() {
-        List<Movie> movies = List.of(
-                new Movie("a", 10),
-                new Movie("b", 15),
-                new Movie("c", 20)
-        );
-
-        movies.stream()
-                .map(movie -> movie.getTitle())
+        var stream = Stream.of(List.of(1, 2, 3), List.of(4, 5, 6));
+        stream
+                .flatMap(list -> list.stream())
                 .forEach(System.out::println);
     }
 }
