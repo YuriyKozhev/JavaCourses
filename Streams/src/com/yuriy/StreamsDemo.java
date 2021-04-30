582de56d0a0bd95d0b96d5822684e5a69807a728
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-01 00:51:14 +0300

    add sorted method example

--- Streams/src/com/yuriy/StreamsDemo.java
+++ Streams/src/com/yuriy/StreamsDemo.java
@@ -3,6 +3,7 @@ package com.yuriy;
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
@@ -10,15 +11,13 @@ import java.util.stream.Stream;
 public class StreamsDemo {
     public static void show() {
         var movies = List.of(
-                new Movie("a", 20),
-                new Movie("b",15),
+                new Movie("b", 20),
+                new Movie("a",15),
                 new Movie("c",25)
         );
 
-        Predicate<Movie> isPopular = movie -> movie.getLikes() > 10;
-
         movies.stream()
-                .takeWhile(movie -> movie.getLikes() > 15)
+                .sorted(Comparator.comparing(Movie::getTitle).reversed())
                 .forEach(movie -> System.out.println(movie.getTitle()));
     }
 }
