4aed2ac2b3de762654e5d1fc0d401021a4c24ac1
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-01 21:11:09 +0300

    add Collectors.groupingBy method example

--- Streams/src/com/yuriy/Movie.java
+++ Streams/src/com/yuriy/Movie.java
@@ -5,10 +5,12 @@ import java.util.Objects;
 public class Movie {
     private String title;
     private int likes;
+    private Genre genre;
 
-    public Movie(String title, int likes) {
+    public Movie(String title, int likes, Genre genre) {
         this.title = title;
         this.likes = likes;
+        this.genre = genre;
     }
 
     public int getLikes() {
@@ -17,6 +19,10 @@ public class Movie {
 
     public String getTitle() {
         return title;
+    }
+
+    public Genre getGenre() {
+        return genre;
     }
 
     @Override
--- Streams/src/com/yuriy/StreamsDemo.java
+++ Streams/src/com/yuriy/StreamsDemo.java
@@ -1,27 +1,20 @@
 package com.yuriy;
 
-import org.w3c.dom.ls.LSOutput;
-
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class StreamsDemo {
     public static void show() {
         var movies = List.of(
-                new Movie("b", 20),
-                new Movie("a",15),
-                new Movie("c",25),
-                new Movie("a",15)
+                new Movie("b", 20, Genre.ACTION),
+                new Movie("a",15, Genre.ACTION),
+                new Movie("c",25, Genre.THRILLER),
+                new Movie("a",15, Genre.COMEDY)
         );
 
-        Map<String, Integer> map = movies.stream()
-                .filter(movie -> movie.getLikes() > 15)
-                .collect(Collectors.toMap(Movie::getTitle, Movie::getLikes));
+        Map<Genre, List<Movie>> map = movies.stream()
+                .collect(Collectors.groupingBy(Movie::getGenre));
 
         System.out.println(map);
 
