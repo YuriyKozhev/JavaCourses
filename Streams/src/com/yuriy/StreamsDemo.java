74febc3f06817d40af483bd38ab8c5ce19c57ebb
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-01 20:57:20 +0300

    add map collect method example

--- Streams/src/com/yuriy/StreamsDemo.java
+++ Streams/src/com/yuriy/StreamsDemo.java
@@ -5,7 +5,9 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamsDemo {
@@ -17,10 +19,11 @@ public class StreamsDemo {
                 new Movie("a",15)
         );
 
-        movies.stream()
-                .map(Movie::getLikes)
-                .reduce((m1, m2) -> m1 + m2)
-                .ifPresent(System.out::println);
+        Map<String, Integer> map = movies.stream()
+                .filter(movie -> movie.getLikes() > 15)
+                .collect(Collectors.toMap(Movie::getTitle, Movie::getLikes));
+
+        System.out.println(map);
 
     }
 }
