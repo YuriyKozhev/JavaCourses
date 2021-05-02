e733df3d2825e726f490fa1a7007549ddb313af4
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-03 00:16:13 +0300

    add join example

--- Concurrency/src/com/yuriy/DownloadFileTask.java
+++ Concurrency/src/com/yuriy/DownloadFileTask.java
@@ -3,6 +3,14 @@ package com.yuriy;
 public class DownloadFileTask implements Runnable {
     @Override
     public void run() {
-        System.out.println("Downloading a file in thread: " + Thread.currentThread().getName());
+        System.out.println("Started downloading a file in thread: " + Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Download complete in thread: " + Thread.currentThread().getName());
     }
 }
