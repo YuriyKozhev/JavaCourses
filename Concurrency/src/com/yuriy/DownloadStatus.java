f17137d609dbe37f5d02bf78e2eaad61de2932eb
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-05 13:22:50 +0300

    add DownloadStatus class with totalBytes parameter

---
+++ Concurrency/src/com/yuriy/DownloadStatus.java
@@ -0,0 +1,13 @@
+package com.yuriy;
+
+public class DownloadStatus {
+    private int totalBytes;
+
+    public int getTotalBytes() {
+        return totalBytes;
+    }
+
+    public void incrementTotalBytes() {
+        totalBytes++;
+    }
+}
