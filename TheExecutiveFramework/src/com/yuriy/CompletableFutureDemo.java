e50cbc72aa95343d0a271ff1a8f954dcb707871d
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-06 16:27:39 +0300

    combine CompletableFutures

--- TheExecutiveFramework/src/com/yuriy/CompletableFutureDemo.java
+++ TheExecutiveFramework/src/com/yuriy/CompletableFutureDemo.java
@@ -3,22 +3,12 @@ package com.yuriy;
 import java.util.concurrent.CompletableFuture;
 
 public class CompletableFutureDemo {
-    public static int toFahrenheit(int celsius) {
-        return (int) (celsius * 1.8) + 32;
-    }
-
-    public static CompletableFuture<String> getUserEmailAsync() {
-        return CompletableFuture.supplyAsync(() -> "email");
-    }
-
-    public static CompletableFuture<String> getPlaylistAsync(String email) {
-        return CompletableFuture.supplyAsync(() -> "playlist");
-    }
-
     public static void show() {
-        getUserEmailAsync()
-                .thenCompose(CompletableFutureDemo::getPlaylistAsync)
-                .thenAccept(System.out::println);
+        var first = CompletableFuture.supplyAsync(() -> "20USD")
+                .thenApply(str -> Integer.parseInt(str.replace("USD", "")));
+        var second = CompletableFuture.supplyAsync(() -> 0.9);
 
+        first.thenCombine(second, (price, exchangeRate) -> price * exchangeRate)
+                .thenAccept(System.out::println);
     }
 }
