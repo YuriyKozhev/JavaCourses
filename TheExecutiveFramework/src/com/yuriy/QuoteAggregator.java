4a53d40de6d9b58f542b1dd8b48490be3deda33a
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-06 17:44:36 +0300

    create QuoteAggregator class with async methods

---
+++ TheExecutiveFramework/src/com/yuriy/QuoteAggregator.java
@@ -0,0 +1,36 @@
+package com.yuriy;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class QuoteAggregator {
+    private String[] sites;
+
+    public QuoteAggregator(String[] sites) {
+        this.sites = sites;
+    }
+
+    public CompletableFuture<Quote> getQuoteAsync(String site) {
+        try {
+            Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 5000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return CompletableFuture
+                .supplyAsync(() -> {
+                    var price = ThreadLocalRandom.current().nextInt(100, 120);
+                    return new Quote(site, price);
+                });
+    }
+
+    public void getAllQuotesAsync() {
+        for (var site :
+                sites) {
+            getQuoteAsync(site)
+                    .thenAccept(System.out::println);
+        }
+    }
+}
+
+
+
