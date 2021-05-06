54259b93cfbbb9b8b661863f964ac15d34bb009b
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-06 13:53:39 +0300

    create ExecutorsDemo class with FixedThreadPool example

---
+++ TheExecutiveFramework/src/com/yuriy/ExecutorsDemo.java
@@ -0,0 +1,12 @@
+package com.yuriy;
+
+import java.util.concurrent.Executors;
+
+public class ExecutorsDemo {
+    public static void show() {
+        var executor = Executors.newFixedThreadPool(2);
+        executor.submit(() -> {
+            System.out.println(Thread.currentThread().getName());
+        });
+    }
+}
