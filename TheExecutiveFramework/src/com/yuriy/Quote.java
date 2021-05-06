108c94c003ab7cd32a4324de50e9b8cf36bf76ec
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-06 17:31:06 +0300

    create class Quote with site and price fields

---
+++ TheExecutiveFramework/src/com/yuriy/Quote.java
@@ -0,0 +1,11 @@
+package com.yuriy;
+
+public class Quote {
+    private String site;
+    private int price;
+
+    public Quote(String site, int price) {
+        this.site = site;
+        this.price = price;
+    }
+}
